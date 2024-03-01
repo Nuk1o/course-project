7aa7ad3c2d25c39c96c0ebfd2791b633e136cfc8
Nukio <NukiXm@yandex.ru>
2024-03-01 03:51:41 +0300

    Save

--- TechTrove/Assets/Scripts/PlayerMovement.cs
+++ TechTrove/Assets/Scripts/PlayerMovement.cs
@@ -5,16 +5,22 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float _moveSpeed;
     [SerializeField] private Rigidbody2D _rigidbody2D;
+    [SerializeField] private Animator _animator;
     private Vector2 _movement;
 
     private void Update()
     {
         _movement.x = Input.GetAxisRaw("Horizontal");
         _movement.y = Input.GetAxisRaw("Vertical");
+        
+        _animator.SetFloat("Horizontal",_movement.x);
+        _animator.SetFloat("Vertical",_movement.y);
+        _animator.SetFloat("Speed",_movement.sqrMagnitude);
+        
     }
 
     private void FixedUpdate()
     {
-        _rigidbody2D.MovePosition(_rigidbody2D.position + _movement * _moveSpeed);
+        _rigidbody2D.MovePosition(_rigidbody2D.position + _movement * _moveSpeed * Time.fixedDeltaTime);
     }
 }
